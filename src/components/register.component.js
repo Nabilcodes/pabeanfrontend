7fc8792fef03d967209fba84117e8cdbc0646d84
Nabilcodes <nabil.muhammad@ui.ac.id>
2024-05-07 23:05:30 +0700

    [REFACTOR] completed signup functionality

--- src/components/register.component.js
+++ src/components/register.component.js
@@ -1,52 +1,163 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class SignUp extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Sign Up</h3>
+// export default class SignUp extends Component {
+//   render() {
+//     return (
+//       <form>
+//         <h3>Sign Up</h3>
 
-        <div className="mb-3">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-          />
-        </div>
+//         <div className="mb-3">
+//           <label>First name</label>
+//           <input
+//             type="text"
+//             className="form-control"
+//             placeholder="First name"
+//           />
+//         </div>
 
-        <div className="mb-3">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
-        </div>
+//         <div className="mb-3">
+//           <label>Last name</label>
+//           <input type="text" className="form-control" placeholder="Last name" />
+//         </div>
 
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
+//         <div className="mb-3">
+//           <label>Email address</label>
+//           <input
+//             type="email"
+//             className="form-control"
+//             placeholder="Enter email"
+//           />
+//         </div>
 
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
+//         <div className="mb-3">
+//           <label>Password</label>
+//           <input
+//             type="password"
+//             className="form-control"
+//             placeholder="Enter password"
+//           />
+//         </div>
 
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Sign Up
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Already registered <a href="/sign-in">sign in?</a>
-        </p>
-      </form>
-    )
-  }
-}
+//         <div className="d-grid">
+//           <button type="submit" className="btn btn-primary">
+//             Sign Up
+//           </button>
+//         </div>
+//         <p className="forgot-password text-right">
+//           Already registered <a href="/sign-in">sign in?</a>
+//         </p>
+//       </form>
+//     )
+//   }
+// }
+
+import React, { useState } from 'react'
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [userName, setUserName] = useState('');
+  // const [address, setAddress] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  // const [about, setAbout] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('https://thingproxy.freeboard.io/fetch/http://35.213.169.90/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password, userName, phoneNumber }),
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Sign Up</h3>
+
+      <div className="mb-3">
+        <label>Email</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label>Username</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Username"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+      </div>
+
+      {/* <div className="mb-3">
+        <label>Address</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Address"
+          value={password}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+      </div> */}
+
+      <div className="mb-3">
+        <label>Phone Number</label>
+        <input
+          type="number"
+          className="form-control"
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+      </div>
+
+      {/* <div className="mb-3">
+        <label>About</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="About"
+          value={password}
+          onChange={(e) => setAbout(e.target.value)}
+        />
+      </div> */}
+
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Sign Up
+        </button>
+      </div>
+      <p className="forgot-password text-right">
+        Already registered <a href="/sign-in">sign in?</a>
+      </p>
+    </form>
+  );
+};
+
+export default SignUp;
