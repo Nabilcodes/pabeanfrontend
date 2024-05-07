258f50c589bad1e20411b8f935287861fe23454a
Nabilcodes <nabil.muhammad@ui.ac.id>
2024-05-07 23:06:51 +0700

    [CHORES] deleted unuseful comment

--- src/components/register.component.js
+++ src/components/register.component.js
@@ -1,56 +1,3 @@
-// import React, { Component } from 'react'
-
-// export default class SignUp extends Component {
-//   render() {
-//     return (
-//       <form>
-//         <h3>Sign Up</h3>
-
-//         <div className="mb-3">
-//           <label>First name</label>
-//           <input
-//             type="text"
-//             className="form-control"
-//             placeholder="First name"
-//           />
-//         </div>
-
-//         <div className="mb-3">
-//           <label>Last name</label>
-//           <input type="text" className="form-control" placeholder="Last name" />
-//         </div>
-
-//         <div className="mb-3">
-//           <label>Email address</label>
-//           <input
-//             type="email"
-//             className="form-control"
-//             placeholder="Enter email"
-//           />
-//         </div>
-
-//         <div className="mb-3">
-//           <label>Password</label>
-//           <input
-//             type="password"
-//             className="form-control"
-//             placeholder="Enter password"
-//           />
-//         </div>
-
-//         <div className="d-grid">
-//           <button type="submit" className="btn btn-primary">
-//             Sign Up
-//           </button>
-//         </div>
-//         <p className="forgot-password text-right">
-//           Already registered <a href="/sign-in">sign in?</a>
-//         </p>
-//       </form>
-//     )
-//   }
-// }
-
 import React, { useState } from 'react'
 
 const SignUp = () => {
@@ -115,17 +62,6 @@ const SignUp = () => {
         />
       </div>
 
-      {/* <div className="mb-3">
-        <label>Address</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Address"
-          value={password}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-      </div> */}
-
       <div className="mb-3">
         <label>Phone Number</label>
         <input
@@ -136,17 +72,6 @@ const SignUp = () => {
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
       </div>
-
-      {/* <div className="mb-3">
-        <label>About</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="About"
-          value={password}
-          onChange={(e) => setAbout(e.target.value)}
-        />
-      </div> */}
 
       <div className="d-grid">
         <button type="submit" className="btn btn-primary">
